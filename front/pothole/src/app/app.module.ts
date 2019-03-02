655144607d3b26a4ab2bc64bc53f12bf6f8dfa45
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2019-03-02 14:40:04 -0500

    Google Map Angular Implemented

--- front/pothole/src/app/app.module.ts
+++ front/pothole/src/app/app.module.ts
@@ -9,10 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AgmCoreModule } from '@agm/core'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AgmCoreModule.forRoot({
+    apiKey: 'AIzaSyDZzqezgaHyYDf7O_UR54-8XvLed5K1sq4'
+    })],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ front/pothole/src/app/map/map.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { MapPage } from './map.page';
+import { AgmCoreModule } from '@agm/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MapPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    AgmCoreModule
+  ],
+  declarations: [MapPage]
+})
+export class MapPageModule {
+  lat:number = 51.678418;
+  lng:number = 7.809007; 
+}
