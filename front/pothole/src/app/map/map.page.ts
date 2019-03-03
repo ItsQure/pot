5a21781c9dcb6fc5ed98550d3e8fa80eadfbd456
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2019-03-03 03:59:00 -0500

    Changed map system

--- front/pothole/src/app/map/map.page.ts
+++ front/pothole/src/app/map/map.page.ts
@@ -106,59 +106,49 @@ export class MapPage implements AfterViewInit {
       //WIP
   };
 
-  Geocode_Parse(addresses) {
-    var currAddress, coords = [];
-    var current_post = new google.maps.LatLng(this.mapCenter.lat, this.mapCenter.lng);
-    coords.push(current_post);
-    for (var i = 0; i < addresses.length; i++) {
-        currAddress = addresses[i];
-        var geocoder = new google.maps.Geocoder();
-        if (geocoder) {
-            geocoder.geocode({'address':currAddress}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    coords.push(results[0].geometry.location);
+  geocodeLatLng(listDest) {
+    var geocoder = new google.maps.Geocoder;
+    var latlng = {lat: this.mapCenter.lat, lng: this.mapCenter.lng};
+    geocoder.geocode({'location': latlng}, function(results, status) {
+      if (status === 'OK') {
+        if (results[0]) {
+          var first = results[0].formatted_address;
+          var base_url = "https://www.google.com/maps/dir/?api=1";
+          var param_url = "";
 
-                    // Check if all calls have been processed
-                    if (coords.length == addresses.length+1) {
-                      //consider the destinations var to be a list of LatLng objects:
-                        var base_url = "https://www.google.com/maps/dir/";
-                        var i;
-                        console.log("geo:");
-                        console.log(coords);
-                        for(i=0; i < coords.length; i++){ 
-                          var lat = coords[i].lat();
-                          var long = coords[i].lng();
-                          console.log("lat: " + lat + " long: " + long);
-                          base_url += lat+","+long+"/";
-                        }
-                        window.open(base_url, '_system');
-                    }
-                }
-            });
+          param_url += "&dir_action=navigate&origin=" + encodeURIComponent(first);
+
+          console.log(listDest);
+
+          if(listDest.length > 1){
+            param_url += "&waypoints=" + encodeURIComponent(listDest[0]);
+          }
+
+          for(var i=1; i < listDest.length-1; i++){ 
+            var place = encodeURIComponent(listDest[i]);
+            param_url += "|" + place;
+          }
+
+          if(listDest.length != 0){
+            param_url += "&destination=" + encodeURIComponent(listDest[listDest.length-1]);
+          }
+
+          base_url += param_url;
+          console.log("base_url: " + base_url);
+          window.open(base_url, '_system');
+        } else {
+          window.alert('No results found');
         }
-    }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    });
   }
 
   clickGoogleMap(){
     //var url = "https://www.google.com/maps/dir/35.9635325,-83.9178258/35.9733887,-83.9322847/35.9639804,-83.9360541/35.96306,-83.9516563/35.987854,-83.9785725/35.9749142,-83.977229";
     //https://www.google.com/maps/dir/35.9625505,-83.91618310000001/35.961694,-83.92322100000001/35.957471,-83.93659500000001/35.9542795,-83.93888520000002/
     //var url = this.createGoogleURL(this.createCoordinates(this.listDests));
-    this.Geocode_Parse(this.listDests);
+    this.geocodeLatLng(this.listDests);
   }
-
-  createGoogleURL(geo_dests){
-      //consider the destinations var to be a list of LatLng objects:
-      var base_url = "https://www.google.com/maps/dir/";
-      var i;
-      console.log("geo:");
-      console.log(geo_dests);
-      for(i=0; i < geo_dests.length; i++){ 
-        var lat = geo_dests[i].lat();
-        var long = geo_dests[i].lng();
-        console.log("lat: " + lat + " long: " + long);
-        base_url += lat+","+long+"/";
-      }
-      return base_url;
-  }
-
 }
