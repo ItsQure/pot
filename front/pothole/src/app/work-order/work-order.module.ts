e4f334b9be44fd250c671e47a79f77eddd0035b2
vtang144 <wph612@vols.utk.edu>
2019-03-02 19:17:00 -0500

    Revert "Importing Cordova code"
    This reverts commit 3ebd5fb29705cc942920955b79767f48bb64bcb3.

--- front/pothole/src/app/work-order/work-order.module.ts
+++ front/pothole/src/app/work-order/work-order.module.ts
@@ -8,6 +8,7 @@ import { IonicModule } from '@ionic/angular';
 import { WorkOrderPage } from './work-order.page';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -21,15 +22,14 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
-  ], 
-  declarations: [WorkOrderPage], 
+  ],
+  declarations: [WorkOrderPage]
 })
 export class WorkOrderPageModule {
   myphoto:any;
   constructor(private camera: Camera) { }
-  captureDataUrl: string;
 
-  capture(){  
+  takePhoto(){  
     const options: CameraOptions = {
     quality: 100,
     destinationType: this.camera.DestinationType.FILE_URI,
@@ -40,7 +40,7 @@ export class WorkOrderPageModule {
     this.camera.getPicture(options).then((imageData) => {
     // imageData is either a base64 encoded string or a file URI
     // If it's base64 (DATA_URL):
-    this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+    this.myphoto = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
     // Handle error
     });
